e851ffc9218592c07d9aaba86670a8bcffee57f3
Akkadius <akkadius1@gmail.com>
2016-10-03 23:01:51 -0500

    Fix 'Click to View' in items display

--- pages/items/item.php
+++ pages/items/item.php
@@ -4,6 +4,7 @@
 
     if(isset($_GET['view_dropped'])) {
         echo return_where_item_dropped($_GET['view_dropped'], 1);
+        exit;
     }
 
     $item_id = (isset($_GET['id']) ? addslashes($_GET['id']) : '');
