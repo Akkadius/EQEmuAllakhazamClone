714e2409ddb2e84c173e5802a03ced73eebdf20d
Akkadius <akkadius1@gmail.com>
2018-06-09 20:32:42 -0500

    Add item search on keypress enter

--- pages/items/items.js
+++ pages/items/items.js
@@ -2,16 +2,16 @@
  * Created by cmiles on 9/18/2016.
  */
 
-function item_search(){
-    u = ".page-content-ajax";
-    query_string = get_form_query_string("item_search_form");
-    console.log(query_string);
-    entire_query = "?a=items_search&v_ajax&" + query_string;
-    push_query = "?a=items_search&" + query_string;
-    $.get(entire_query, function (data) {
-        history.pushState('page_pop', push_query, push_query);
-        $(u).html(data);
-    });
+function item_search() {
+	u = ".page-content-ajax";
+	query_string = get_form_query_string("item_search_form");
+	console.log(query_string);
+	entire_query = "?a=items_search&v_ajax&" + query_string;
+	push_query = "?a=items_search&" + query_string;
+	$.get(entire_query, function (data) {
+		history.pushState('page_pop', push_query, push_query);
+		$(u).html(data);
+	});
 }
 
 function npc_dropped_view(item_id) {
@@ -22,6 +22,14 @@ function npc_dropped_view(item_id) {
 	});
 }
 
+$('#iname').keypress(function (e) {
+	var key = e.which;
+	if (key === 13)  // the enter key code
+	{
+		item_search();
+		return false;
+	}
+});
 /*
 $(document).on("change", "#item_search_form input, #item_search_form select", function(e){
     item_search();
