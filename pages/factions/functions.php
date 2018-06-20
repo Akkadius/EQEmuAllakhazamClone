69dcab8357acbb01f3d8d10deec32ebb723b3eff
Akkadius <akkadius1@gmail.com>
2018-06-19 23:12:33 -0500

    Adjust print_npcs_by_zone

--- pages/factions/functions.php
+++ pages/factions/functions.php
@@ -8,10 +8,10 @@ function print_npcs_by_zone($query_result)
             if ($current_zone_iteration != $row["zone"]) {
                 if ($current_zone_iteration != "")
                     $print_buffer .= "                  <br/><br/>\n";
-                $print_buffer .= "                  <b>in <a href='?a=zone&name=" . $row["zone"] . "'>" . $row["long_name"] . "</a> by </b>\n";
+                $print_buffer .= "                  <b>in <a href='?a=zone&name=" . $row["zone"] . "'>" . $row["long_name"] . "</a> by </b><br>";
                 $current_zone_iteration = $row["zone"];
             }
-            $print_buffer .= "<li><a href='?a=npc&id=" . $row["id"] . "'>" . str_replace("_", " ", $row["name"]) . "</a> (" . $row["id"] . ")</li>\n";
+            $print_buffer .= "<li style='list-style-type:none; margin-left: 15px;'><a href='?a=npc&id=" . $row["id"] . "'>" . str_replace("_", " ", $row["name"]) . "</a> (" . $row["id"] . ")</li>\n";
         }
         if ($current_zone_iteration != "")
             $print_buffer .= "                  <br/><br/>\n";
